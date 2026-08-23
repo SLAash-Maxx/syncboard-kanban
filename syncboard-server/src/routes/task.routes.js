a5a83d1fbc35718db6a0c7c69408aef9959b15b7
Senanayaka25 <chamodsenanayaka25@gmail.com>
2026-08-23 11:35:11 +0530

    Add task routes with authentication middleware

--- syncboard-server/src/routes/task.routes.js
+++ syncboard-server/src/routes/task.routes.js
@@ -9,3 +9,12 @@ const {
 const { requireAuth } = require('../middleware/auth.middleware');
 
 const router = express.Router();
+router.use(requireAuth);
+
+router.get('/', getTasks);
+router.get('/:id', getTask);
+router.post('/', createTask);
+router.patch('/:id', updateTask);
+router.delete('/:id', deleteTask);
+
+module.exports = router;
