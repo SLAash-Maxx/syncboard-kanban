e1443ca85aee9cc126ced6cee3802f9f762be244
ChaRithMrAJaPaKsha <charith2567@gmail.com>
2026-08-23 16:09:53 +0530

    Add task API helpers
    Add shared client helpers for fetching the task list and creating a new task. Both methods pass the auth token through the existing request utility so the client can call the task endpoints consistently.

--- syncboard-client/src/api/taskApi.js
+++ syncboard-client/src/api/taskApi.js
@@ -1,1 +1,9 @@
 import request from './client';
+
+export function getTasks(token) {
+  return request('/tasks', { token }).then((data) => data.tasks);
+}
+
+export function createTask(task, token) {
+  return request('/tasks', { method: 'POST', body: task, token }).then((data) => data.task);
+}
